0664bda88eef3176facbab3bf5365d22d94e8116
Sofia <burovasofia@list.ru>
2022-07-11 17:14:51 +0300

    шестой коммит

---
+++ pro001/pro006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username=="Маша")
+{
+    Console.WriteLine("Ура! Это же маша");
+}
+else
+{
+   Console.Write("Привет, "); 
+   Console.WriteLine(username);
+}
+
